8ad9f5a575b566a3c2792e8a8dc190694c31b94d
brantje <brantje@gmail.com>
2015-02-07 11:13:36 +0100

    Fix to prevent a null check from crashing the running node server

--- bootstrap.js
+++ bootstrap.js
@@ -17,17 +17,17 @@ mongoose.connection.on('open', function (err) {
         mongoose.connection.db.authenticate(config.mongodb.user, config.mongodb.password, function(err) {
           if (!err) return;
           console.error(err);
-          process.exit(1);
+          //process.exit(1);
         });
         return;
       } else {
         console.error(err);
-        process.exit(1);
+        //process.exit(1);
       }
     }
     if (!semver.satisfies(data.version, '>=2.1.0')) {
       console.error('Error: Uptime requires MongoDB v2.1 minimum. The current MongoDB server uses only '+ data.version);
-      process.exit(1);
+      //process.exit(1);
     }
   });
 });
