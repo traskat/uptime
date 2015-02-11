d4af4360556f966201395f2b22a8bd25441237e2
brantje <brantje@gmail.com>
2015-02-11 20:21:56 +0100

    Auto connect on mongo connection loss

--- bootstrap.js
+++ bootstrap.js
@@ -8,34 +8,36 @@ var userAuthStr = '';
 if (config.mongodb.user) {
   userAuthStr = config.mongodb.user + ':' + config.mongodb.password + '@';
 }
-mongoose.connect(config.mongodb.connectionString || 'mongodb://' + userAuthStr + config.mongodb.server +'/' + config.mongodb.database);
-mongoose.connection.on('error', function (err) {
-  console.error('MongoDB error: ' + err.message);
-  console.error('Make sure a mongoDB server is running and accessible by this application');
-  process.exit(1);
-});
-mongoose.connection.on('open', function (err) {
-  mongoose.connection.db.admin().serverStatus(function(err, data) {
-    if (err) {
-      if (err.name === "MongoError" && (err.errmsg === 'need to login' || err.errmsg === 'unauthorized') && !config.mongodb.connectionString) {
-        console.log('Forcing MongoDB authentication');
-        mongoose.connection.db.authenticate(config.mongodb.user, config.mongodb.password, function(err) {
-          if (!err) return;
-          console.error(err);
-          //process.exit(1);
+var connectWithRetry = function() {
+  mongoose.connect(config.mongodb.connectionString || 'mongodb://' + userAuthStr + config.mongodb.server + '/' + config.mongodb.database,{server: { auto_reconnect: true}},function(err){
+    if(err){
+      console.error('Failed to connect to mongo on startup - retrying in 5 sec', err);
+      connectWithRetry()
+    } else {
+      mongoose.connection.on('open', function (err) {
+        mongoose.connection.db.admin().serverStatus(function (err, data) {
+          if (err) {
+            if (err.name === "MongoError" && (err.errmsg === 'need to login' || err.errmsg === 'unauthorized') && !config.mongodb.connectionString) {
+              console.log('Forcing MongoDB authentication');
+              mongoose.connection.db.authenticate(config.mongodb.user, config.mongodb.password, function (err) {
+                if (!err) return;
+                console.error(err);
+                //process.exit(1);
+              });
+              return;
+            } else {
+              console.error(err);
+              //process.exit(1);
+            }
+          }
+          if (!semver.satisfies(data.version, '>=2.1.0')) {
+            console.error('Error: Uptime requires MongoDB v2.1 minimum. The current MongoDB server uses only ' + data.version);
+            //process.exit(1);
+          }
         });
-        return;
-      } else {
-        console.error(err);
-        //process.exit(1);
-      }
-    }
-    if (!semver.satisfies(data.version, '>=2.1.0')) {
-      console.error('Error: Uptime requires MongoDB v2.1 minimum. The current MongoDB server uses only '+ data.version);
-      //process.exit(1);
+      });
     }
   });
-});
-
-
+};
+connectWithRetry();
 module.exports = mongoose;
